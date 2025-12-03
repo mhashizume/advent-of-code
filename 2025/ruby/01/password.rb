102b222aafa09fb708b71ab56f23ae53012a67fb
Michael Hashizume <michael.hashizume@gmail.com>
2025-12-02 21:26:05 -0800

    Day 1 part 1 solution

--- 2025/ruby/01/password.rb
+++ 2025/ruby/01/password.rb
@@ -5,21 +5,12 @@ pass = 0
 
 File.readlines('./input').each do |line|
   number = line.match(/[0-9]+/).to_s.to_i
-  require 'debug'; binding.b if number > 100
   if line.start_with?('L')
-    if (dial - number).negative?
-      dial = 100 + (dial - number)
-    else
-      dial -= number
-    end
-  elsif dial + number > 99
-    dial = dial + number - 100
+    dial = (dial - number) % 100
   else
-    dial += number
+    dial = (dial + number) % 100
   end
 
-#   require 'debug'; binding.b if dial.zero?
-#   pp "Dial is #{dial}"
   pass += 1 if dial.zero?
 end
 
